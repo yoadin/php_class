c47187af485f24f74acde44f66796bb7b9d34e30
yoadin <kalkidanasdro11@gmail.com>
2025-11-25 12:20:22 +0300

    constant declaring using 'const' key word.

---
+++ ex9.php
@@ -0,0 +1,11 @@
+<?php
+
+const APP_NAME = 'Student Portal';
+
+const VERSION = '1.0';
+
+const AUTHOR = 'Kalkidan Asdro';
+
+echo "Welcome to ". APP_NAME . " (version ". VERSION. " ) developed by ". AUTHOR. ".";
+
+?>
